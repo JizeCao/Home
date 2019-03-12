fc1c3ef63e86881851d2383159f87dc8fc2d2bd8
ximinglu <lux32@uw.edu>
2019-03-12 01:27:10 -0700

    update

--- model.py
+++ model.py
@@ -73,7 +73,7 @@ class Model(torch.nn.Module):
             obj[0, 0] = 1
         if bowl:
             obj[0, 1] = 1
-        additional_score = self.augmented_linear(torch.from_numpy(obj))
+        additional_score = self.augmented_linear(torch.from_numpy(obj).cuda())
         augmented_x = self.augmented_combination(torch.cat([x, additional_score]))
         return augmented_x
 
